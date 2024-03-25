0db1108f2e85a9d8bf1a9f5cfc7eedb900a5e5c2
Victor Ren <victorren2002@gmail.com>
2024-03-25 14:35:16 -0700

    added poll routes get

--- backend/api/routes/poll.routes.js
+++ backend/api/routes/poll.routes.js
@@ -1,5 +1,18 @@
+// Sends some sample data to mongodb server
+const mongoose = require('mongoose');
+require('dotenv').config();
+const User = require("../../models/user.js");
+const Poll = require("../../models/poll.js")
 const express = require("express");
 const router = express.Router();
+
+
+// Connect to mongodb
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 router.patch('/vote/', (req, res) => {
 
@@ -17,9 +30,20 @@ router.post('/', (req, res) => {
     
 })
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    res.send("Get user " + id);
+router.get('/:id', async (req, res) => {
+    const _id = req.params.id;
+    try{
+        const poll = await Poll.findById(_id);
+        if (!poll) {
+            return res.status(404).send({ message: 'Poll not found' });
+        }
+        else {
+           res.send(poll);
+        }
+    } catch (error) {
+        // If there's an error, it might be because the `id` is not a valid ObjectId
+        res.status(500).send({ message: error.message });
+    }
 })
 
 module.exports = router;
