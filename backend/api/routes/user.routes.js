30b56f6aa1ae4d41351924bf09a3b13bc5ed997a
cbaeucsd <102708712+cbaeucsd@users.noreply.github.com>
2024-03-25 14:24:48 -0700

    user.routes.js starter

---
+++ backend/api/routes/user.routes.js
@@ -0,0 +1,10 @@
+// Imports
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+router.post('/signup/', (req, res) => {});
+
+router.post('/login/', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
