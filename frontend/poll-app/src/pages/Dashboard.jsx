fbcc33b7d406d34e1c3a028846ca48b93d966afa
cbaeucsd <102708712+cbaeucsd@users.noreply.github.com>
2024-03-26 23:47:36 -0700

    Merge branch 'main' of https://github.com/VdotR/polling_system

---
+++ frontend/poll-app/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Page from '../components/page'
+
+function Dashboard() {
+    return (
+        <Page title="Dashboard">
+            <h2>Something idk </h2>
+        </Page>
+    );
+}
+
+export default Dashboard;
