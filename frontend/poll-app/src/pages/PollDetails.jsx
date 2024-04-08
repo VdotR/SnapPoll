9144b67a0fe227209ed8bfa18eb230ea6e51c8a8
David Tran <tr.davidt@gmail.com>
2024-04-08 14:54:26 -0700

    show shortid in dialog with qr code on poll details

--- frontend/poll-app/src/pages/PollDetails.jsx
+++ frontend/poll-app/src/pages/PollDetails.jsx
@@ -4,11 +4,13 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useUserContext } from '../../context';
 import Loading from '../components/loading';
 import config from '../config';
-import { clearPollRequest, truncate } from '../utils/pollUtils';
+import { clearPollRequest, getDialogText, truncate } from '../utils/pollUtils';
 import { Chart as ChartJS } from 'chart.js/auto'; // needed for some reason
 import { Bar } from 'react-chartjs-2';
-import { FaRedo, FaEraser, FaTrashAlt } from 'react-icons/fa';
+import { FaRedo, FaEraser, FaExpandAlt } from 'react-icons/fa';
 import '../css/PollDetails.css';
+import Dialog from '../components/dialog';
+import QRCode from 'react-qr-code';
 
 function PollDetails() {
     const navigate = useNavigate();
@@ -17,6 +19,7 @@ function PollDetails() {
     const [poll, setPoll] = useState(null);
     const [counts, setCounts] = useState({});
     const [correctOption, setCorrectOption] = useState(null);
+    const [showCorrectOption, setShowCorrectOption] = useState(false);
 
     const correctOptionStyle = {
         backgroundColor: "#dff0d8"
@@ -34,23 +37,43 @@ function PollDetails() {
             y: {
                 // max: Math.max.apply(null, Object.values(counts)) + 1, 
                 ticks: {
-                beginAtZero: true,
-                stepSize: 1
+                    beginAtZero: true,
+                    stepSize: 1,
+                    font: {
+                        size: 16
+                    }
+                }
+            },
+            x: {
+                ticks: {
+                    font: {
+                        size: 16,
+                    }
                 }
             }
-        }    
+        }
     }
 
+    function mapColorIfCorrect(data, correctColor, defaultColor) {
+        return data.map((_, index) => {
+            if (index === correctOption && showCorrectOption) {
+                return correctColor;
+            }
+            return defaultColor;
+        })
+    }
+
+    const chartLabels = Object.keys(counts)
     const chartData = {
-        labels: Object.keys(counts),
+        labels: chartLabels,
         datasets: [
             {
                 label: 'Responses',
-                backgroundColor: Object.keys(counts).map((_, index) => index === correctOption ? config.BAR_GREEN : config.BAR_GREY),
-                borderColor: Object.keys(counts).map((_, index) => index === correctOption ? config.BAR_GREEN_BORDER : config.BAR_GREY_BORDER),
+                backgroundColor: mapColorIfCorrect(chartLabels, config.BAR_GREEN, config.BAR_GREY),
+                borderColor: mapColorIfCorrect(chartLabels, config.BAR_GREEN_BORDER, config.BAR_GREY_BORDER),
                 borderWidth: 1,
-                hoverBackgroundColor: Object.keys(counts).map((_, index) => index === correctOption ? config.BAR_GREEN_HOVER : config.BAR_GREY_HOVER),
-                hoverBorderColor: Object.keys(counts).map((_, index) => index === correctOption ? config.BAR_GREEN_BORDER : config.BAR_GREY_BORDER), // For now same as not hovered
+                hoverBackgroundColor: mapColorIfCorrect(chartLabels, config.BAR_GREEN_HOVER, config.BAR_GREY_HOVER),
+                hoverBorderColor: mapColorIfCorrect(chartLabels, config.BAR_GREEN_BORDER, config.BAR_GREY_BORDER), // For now same as not hovered
                 data: counts
             }
         ]
@@ -80,6 +103,7 @@ function PollDetails() {
             return res.json();
         })
         .then(data => {
+            console.log(data)
             setPoll(data);
             setCounts(countResponses(data));
             setCorrectOption(data.correct_option);
@@ -102,10 +126,12 @@ function PollDetails() {
         setCounts(newCounts);
     }
     
-    // 
     useEffect(() => {
         if (!poll) fetchPoll(poll_id);
     }, []);
+
+    const presentBtn = <button><FaExpandAlt /><span>Present</span></button>;
+    const clearBtn = <button><FaEraser /><span>Clear</span></button>;
 
     return (
         <Page title={`Responses for \"${poll == null? '...' : truncate(poll.question)}\"`}>
@@ -120,7 +146,25 @@ function PollDetails() {
                     <div className='table-outer-container'>
                         <div className='toolbar'>
                             <button onClick={() => fetchPoll(poll_id)}><FaRedo /></button>
-                            <button onClick={() => {clearPoll(poll)}}><FaEraser /> Clear responses</button>
+                            <Dialog
+                                title={"Vote now at <domain>/vote"}
+                                text={<>
+                                    {poll.shortId}
+                                    <QRCode value={`http://localhost:5173/vote/${poll.shortId}`} />
+                                </>}
+                                target={presentBtn}
+                                big
+                            />
+                            <Dialog 
+                                title={"Confirm clear poll"}
+                                text={getDialogText(`clear the responses for "${truncate(poll.question)}"`)}
+                                target={clearBtn}
+                                onConfirm={() => clearPoll(poll)}
+                            />
+                            <span className='toolbar-checkbox'>
+                                <input type="checkbox" defaultChecked={showCorrectOption} onChange={() => setShowCorrectOption(!showCorrectOption)} /> 
+                                <span>Show correct option</span>
+                            </span>                   
                         </div>
                         <div className='table-container'>
                             <table>
@@ -133,7 +177,7 @@ function PollDetails() {
                                 </thead>
                                 <tbody>
                                     {Object.keys(counts).map(option => {
-                                        return <tr key={option} style={poll.options.indexOf(option) == correctOption ? correctOptionStyle : {}}>
+                                        return <tr key={option} style={poll.options.indexOf(option) == correctOption && showCorrectOption ? correctOptionStyle : {}}>
                                             <td>{truncate(option)}</td>
                                             <td>{counts[option]}</td>
                                             <td>{(counts[option] / poll.responses.length * 100).toFixed(2) + "%"}</td>
